100b8d8e97820d4b812fefe30d8d095ce3877ad8
Rajat Sirohi <sirohirajat6@gmail.com>
2019-02-14 00:25:37 -0500

    updated saved files

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Chaos Game
 
-![alt image](Saved/z.gif?raw=true "ST")
+![alt image](Saved/ST_10_5.gif?raw=true "ST")
+![alt image](Saved/SQ_10_4.gif?raw=true "SQ")
+![alt image](Saved/PENT2_10_5.gif?raw=true "PENT2")
 
-Cool diagram above
+Cool diagrams above
---
+++ chaos_game_PENT1.py
@@ -0,0 +1,60 @@
+'''
+This runs the chaos game for a pentagon
+where the current vertex cannot be the
+same as the previous vertex
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from math import sqrt
+
+def frac_point(p1,i1,p2,i2,r=0.5):
+    x1 = p1[0][i1]
+    y1 = p1[1][i1]
+    x2 = p2[0][i2]
+    y2 = p2[1][i2]
+
+    xp = x1*(1-r) + x2*r
+    yp = y1*(1-r) + y2*r
+    return xp,yp
+
+randv = -1
+temp = -1
+def animate(i):
+    global randv,temp
+    if ((i+1)/N*100)%k==0:
+        print(str((i+1)/N*100)+"% Complete")
+    
+    if i != 0:
+        randv = np.random.randint(0,v)
+        while randv == temp:
+            randv = np.random.randint(0,v)
+        temp = randv
+        
+        pt = frac_point(p,i-1,vertices,randv)
+        p[0].append(pt[0])
+        p[1].append(pt[1])
+        sc.set_offsets(np.c_[p[0],p[1]])
+
+        # print("["+str(i)+"] ("+str(p[0][i-1])+","+str(p[1][i-1])+") --> ("+
+        #       str(vertices[0][randv])+","+str(vertices[1][randv])+") = "+str(pt))
+    return sc
+
+N = 10**4
+k = 1
+dotSize = 10**4/N
+
+xvertices = [.2,0,.5,1,.8]
+yvertices = [0,.7,1,.7,0]
+vertices = [xvertices,yvertices]
+v = len(xvertices)
+
+fig, ax = plt.subplots()
+p = [[np.random.random()],[np.random.random()]]
+sc = ax.scatter(p[0],p[1],marker='.',color='purple',s=dotSize)
+plt.xlim(0,1)
+plt.ylim(0,1)
+
+ani = FuncAnimation(fig,animate,interval=10,frames=N)
+ani.save('chaos_game_pentagon1.mp4',fps=N/10,dpi=80)
